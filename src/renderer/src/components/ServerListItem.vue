6858ef2bf2e4318e91841c47e4569539b3267e13
Luis Gustavo S. Barreto <gustavo@ossystems.com.br>
2024-03-18 23:46:18 -0300

    nada

---
+++ src/renderer/src/components/ServerListItem.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-list-item :value="item" lines="two" v-bind="props">
+    <v-list-item-subtitle>
+      {{ item.url }}
+    </v-list-item-subtitle>
+
+    <template #prepend>
+      <div class="mr-4">
+        <v-chip size="x-small" color="primary">v0.13.0</v-chip>
+      </div>
+    </template>
+    <template #append>
+      <v-btn
+        :disabled="item.permanent"
+        icon="mdi-delete"
+        variant="plain"
+        @click.stop="removeInstance(index)"
+      ></v-btn>
+    </template>
+  </v-list-item>
+</template>
+
+<script setup lang="ts">
+import { PropType } from 'vue'
+import { useAppStore, Instance } from '../stores'
+
+const store = useAppStore()
+
+const props = defineProps({
+  index: {
+    type: Number,
+    required: true
+  },
+  item: {
+    type: Object as PropType<Instance>,
+    required: true
+  },
+});
+
+const removeInstance = (index: number) => {
+  // If the element is currently selected, select the previous element
+  if (store.instances[index] === store.selectedInstance) {
+    store.selectInstance(store.instances[index - 1])
+  }
+
+  store.deleteInstance(index)
+}
+</script>
--- src/renderer/src/views/Login.vue
+++ src/renderer/src/views/Login.vue
@@ -1,30 +1,10 @@
 <template>
   <v-main class="d-flex">
-    <v-container
-      class="full-height d-flex justify-center align-center"
-      fluid
-    >
-      <v-row
-        align="center"
-        justify="center"
-      >
-        <v-col
-          cols="12"
-          sm="8"
-          md="4"
-          xl="3"
-        >
-          <transition
-            name="bounce"
-            appear
-            @after-leave="openWebView"
-          >
-            <v-card
-              v-if="show"
-              theme="dark"
-              class="pa-6 bg-v-theme-surface"
-              rounded="lg"
-            >
+    <v-container class="full-height d-flex justify-center align-center" fluid>
+      <v-row align="center" justify="center">
+        <v-col cols="12" sm="8" md="4" xl="3">
+          <transition name="bounce" appear @after-leave="openWebView">
+            <v-card v-if="show" theme="dark" class="pa-6 bg-v-theme-surface" rounded="lg">
               <v-card-title class="d-flex justify-center align-center mt-4">
                 <v-img
                   :src="Logo"
@@ -32,39 +12,21 @@
                   alt="ShellHub logo, a cloud with a shell in your base write ShellHub in the right side"
                 />
               </v-card-title>
-              <p class="text-caption text-center text-md font-weight-bolad">Desktop Application
-              </p>
+              <p class="text-caption text-center text-md font-weight-bolad">Desktop Application</p>
 
               <v-container>
                 <v-slide-y-reverse-transition>
-                  <v-alert
-                    v-model="showError"
-                    type="error"
-                    closable
-                    variant="tonal"
-                    class="mb-4"
-                  >
+                  <v-alert v-model="showError" type="error" closable variant="tonal" class="mb-4">
                     <strong>Invalid login credentials:</strong>
                     Your password is incorrect or this account doesn't exists.
                   </v-alert>
                 </v-slide-y-reverse-transition>
 
-                <v-dialog
-                  v-model="showDialog"
-                  width="400"
-                >
-                  <v-card
-                    class="bg-v-theme-surface"
-                    data-test="deviceRename-card"
-                  >
-                    <v-card-title class="text-h5 pa-5 bg-primary">
-                      Add Instance
-                    </v-card-title>
+                <v-dialog v-model="showDialog" width="400">
+                  <v-card class="bg-v-theme-surface" data-test="deviceRename-card">
+                    <v-card-title class="text-h5 pa-5 bg-primary"> Add Instance </v-card-title>
                     <v-divider />
-                    <v-form
-                      @submit.prevent="addInstance"
-                      class="ma-2"
-                    >
+                    <v-form @submit.prevent="addInstance" class="ma-2">
                       <v-col>
                         <v-text-field
                           v-model="newInstanceForm.name"
@@ -85,25 +47,14 @@
                           data-test="password-text"
                         />
                         <v-card-actions class="justify-center pa-0">
-                          <v-btn
-                            color="primary"
-                            block
-                            type="submit"
-                            variant="flat"
-                          >
-                            SAVE
-                          </v-btn>
+                          <v-btn color="primary" block type="submit" variant="flat">SAVE</v-btn>
                         </v-card-actions>
-
                       </v-col>
                     </v-form>
                   </v-card>
                 </v-dialog>
 
-                <v-form
-                  v-model="validForm"
-                  @submit.prevent="login"
-                >
+                <v-form v-model="validForm" @submit.prevent="login">
                   <v-col>
                     <v-combobox
                       v-model="instance"
@@ -115,32 +66,11 @@
                       :items="items"
                     >
                       <template #item="{ item, index, props }">
-                        <v-list-item
-                          :value="item.value"
-                          lines="two"
+                        <ServerListItem
                           v-bind="props"
-                        >
-                          <v-list-item-subtitle>
-                            {{ item.raw.url }}
-                          </v-list-item-subtitle>
-
-                          <template #prepend>
-                            <div class="mr-4">
-                              <v-chip
-                                size="x-small"
-                                color="primary"
-                              >v0.13.0</v-chip>
-                            </div>
-                          </template>
-                          <template #append>
-                            <v-btn
-                              :disabled="item.raw.permanent"
-                              icon="mdi-delete"
-                              variant="plain"
-                              @click.stop="removeInstance(index)"
-                            ></v-btn>
-                          </template>
-                        </v-list-item>
+                          :index="index"
+                          :item="item.raw"
+                        ></ServerListItem>
                       </template>
 
                       <template #append-item>
@@ -151,7 +81,9 @@
                           variant="text"
                           class="mt-2"
                           @click="showDialog = true"
-                        >Add Custom Instance</v-btn>
+                        >
+                          Add Custom Instance
+                        </v-btn>
                       </template>
                     </v-combobox>
 
@@ -167,11 +99,7 @@
                       </v-btn>
                     </v-card-actions>
 
-                    <v-progress-linear
-                      v-show="connecting"
-                      indeterminate
-                      class="mt-2"
-                    />
+                    <v-progress-linear v-show="connecting" indeterminate class="mt-2" />
                   </v-col>
                 </v-form>
               </v-container>
@@ -184,12 +112,14 @@
 </template>
 <script setup lang="ts">
 import { reactive, computed } from "vue";
-import { ref } from "vue";
+import { ref } from 'vue'
 import { useRouter } from 'vue-router'
 import { useAppStore, Instance } from '../stores'
 
-import Logo from "../assets/logo-inverted.png"
-import { onMounted } from "vue";
+import ServerListItem from '../components/ServerListItem.vue'
+
+import Logo from '../assets/logo-inverted.png'
+import { onMounted } from 'vue'
 
 const store = useAppStore()
 const router = useRouter()
@@ -200,7 +130,7 @@ const showDialog = ref(false)
 const newInstanceForm = reactive({ name: '', url: '' })
 const show = ref(true)
 
-const items = store.instances;
+const items = store.instances
 const instance = computed({
   get(): Instance {
     return store.selectedInstance
@@ -235,15 +165,6 @@ const addInstance = () => {
 
   // Select the newly added instance
   store.selectInstance(store.instances[store.instances.length - 1])
-}
-
-const removeInstance = (index: number) => {
-  // If the element is currently selected, select the previous element
-  if (store.instances[index] === store.selectedInstance) {
-    store.selectInstance(store.instances[index - 1])
-  }
-
-  store.deleteInstance(index)
 }
 
 onMounted(() => {
