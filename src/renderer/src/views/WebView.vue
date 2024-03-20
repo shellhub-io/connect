691dbc7a23cd030a08280860f3e2e0e7c6c2994a
Luis Gustavo S. Barreto <gustavo@ossystems.com.br>
2024-03-20 11:51:19 -0300

    tabs

--- src/renderer/src/views/WebView.vue
+++ src/renderer/src/views/WebView.vue
@@ -1,77 +1,134 @@
 <template>
-  <transition name="fade" appear>
-    <v-app-bar color="primary" density="default" extended extension-height="48">
-      <template #prepend>
-        <v-menu>
-          <template #activator="{ props }">
-            <v-app-bar-nav-icon v-bind="props" variant="text">
-              <v-icon>mdi-server</v-icon>
-            </v-app-bar-nav-icon>
-          </template>
-          <v-list v-model:selected="instance" class="pa-0">
-            <ServerListItem
-              v-for="(item, index) in items"
-              :key="index"
-              :index="index"
-              :item="item"
-            ></ServerListItem>
-          </v-list>
-        </v-menu>
-      </template>
-
-      <v-btn-group color="primary" :border="true">
-        <v-btn icon="mdi-arrow-left-bold"></v-btn>
-        <v-btn icon="mdi-arrow-right-bold"></v-btn>
-        <v-divider vertical></v-divider>
-        <v-btn icon="mdi-refresh" @click="reload"></v-btn>
-      </v-btn-group>
-      <v-text-field
-        v-model="url"
-        readonly
-        hide-details
-        single-line
-        variant="solo"
-        density="comfortable"
-        class="ml-2"
-        :loading="isLoading"
+  <transition
+    name="fade"
+    appear
+  >
+    <v-app-bar
+      color=""
+      density="compact"
+      extended
+      extension-height="78"
+    >
+      <v-tabs
+        grow
+        class="ml-0" hide-slider bg-color="surface" density="comfortable"
       >
-        <template #prepend-inner>
-          <v-icon :color="url.startsWith('https') ? 'success' : 'error'">
-            {{ url.startsWith('https') ? 'mdi-lock' : 'mdi-lock-off' }}
-          </v-icon>
-        </template>
-      </v-text-field>
+        <v-tab class="text-subtitle-1">
+          ShellHub Cloud
+          <template #prepend>
+            <v-btn
+              icon="mdi-cloud"
+              size="small"
+            ></v-btn>
+          </template>
+          <template #append>
+            <v-btn
+              icon="mdi-close"
+              size="x-small"
+            ></v-btn>
+          </template>
+        </v-tab>
+        <v-tab class="text-subtitle-1">
+          Self-hosted Community
+          <template #prepend>
+            <v-btn
+              icon="mdi-cloud"
+              size="small"
+            ></v-btn>
+          </template>
+          <template #append>
+            <v-btn
+              icon="mdi-close"
+              size="x-small"
+            ></v-btn>
+          </template>
+        </v-tab>
+        <v-tab class="text-subtitle-1">
+          Enterprise Edition
+          <template #prepend>
+            <v-btn
+              icon="mdi-cloud"
+              size="small"
+            ></v-btn>
+          </template>
+          <template #append>
+            <v-btn
+              icon="mdi-close"
+              size="x-small"
+            ></v-btn>
+          </template>
+        </v-tab>
+      </v-tabs>
 
-      <v-btn class="ml-2" variant="text" icon="mdi-plus-box" @click="reload"></v-btn>
 
       <template #extension>
-        <v-tabs grow class="ml-0">
-          <v-tab class="text-subtitle-1">
-            ShellHub Cloud
-            <template #prepend>
-              <v-btn icon="mdi-cloud" size="small"></v-btn>
+          <v-menu>
+            <template #activator="{ props }">
+              <v-app-bar-nav-icon
+                v-bind="props"
+                variant="text"
+              >
+                <v-icon>mdi-server</v-icon>
+              </v-app-bar-nav-icon>
             </template>
-            <template #append>
-              <v-btn icon="mdi-close" size="x-small"></v-btn>
-            </template>
-          </v-tab>
-          <v-tab class="text-subtitle-1">
-            Self-hosted Community
-            <template #prepend>
-              <v-btn icon="mdi-cloud" size="small"></v-btn>
-            </template>
-            <template #append>
-              <v-btn icon="mdi-close" size="x-small"></v-btn>
-            </template>
-          </v-tab>
-        </v-tabs>
-        <v-divider vertical></v-divider>
-        <v-btn icon="mdi-plus-box"></v-btn>
+            <v-list
+              v-model:selected="instance"
+              class="pa-0"
+            >
+              <ServerListItem
+                v-for="(item, index) in items"
+                :key="index"
+                :index="index"
+                :item="item"
+              ></ServerListItem>
+            </v-list>
+          </v-menu>
+
+        <v-btn-group
+          color="primary"
+          :border="true"
+        >
+          <v-btn icon="mdi-arrow-left-bold"></v-btn>
+          <v-btn icon="mdi-arrow-right-bold"></v-btn>
+          <v-divider vertical></v-divider>
+          <v-btn
+            icon="mdi-refresh"
+            @click="reload"
+          ></v-btn>
+        </v-btn-group>
+        <v-text-field
+          v-model="url"
+          readonly
+          hide-details
+          single-line
+          variant=""
+          density="comfortable"
+          class="ml-2 bg-v-theme-surface"
+          bg-color=""
+          :loading="isLoading"
+        >
+          <template #prepend-inner>
+            <v-icon :color="url.startsWith('https') ? 'success' : 'error'">
+              {{ url.startsWith('https') ? 'mdi-lock' : 'mdi-lock-off' }}
+            </v-icon>
+          </template>
+        </v-text-field>
+
+        <v-btn
+          class="ml-2"
+          variant="text"
+          icon="mdi-plus-box"
+          @click="reload"
+        ></v-btn>
       </template>
     </v-app-bar>
   </transition>
   <v-main>
-    <v-window v-model="window" class="fill-height" direction="vertical">
+    <v-window
+      v-model="window"
+      class="fill-height"
+      direction="vertical"
+    >
       <v-window-item
         v-for="(item, index) in items"
         :key="item.url"
@@ -89,9 +146,12 @@
   </v-main>
 </template>
 
-<script setup lang="ts">
+<script
+  setup
+  lang="ts"
+>
 import { DidStartNavigationEvent } from 'electron'
-import { ref, onMounted, onBeforeUnmount, computed, Ref  } from 'vue'
+import { ref, onMounted, onBeforeUnmount, computed, Ref } from 'vue'
 import { Instance, useAppStore } from '../stores'
 import ServerListItem from '@renderer/components/ServerListItem.vue'
 import { useRefreshableComputed } from '@renderer/lib/useRefreshable'
@@ -168,16 +228,19 @@ onMounted(() => {
 .v-tab {
   text-transform: none !important;
   justify-content: stretch;
- /* background-color: rgb(var(--v-theme-background));*/
+  /* background-color: rgb(var(--v-theme-background));*/
 }
 
->>> .v-tab .v-btn__content {
+.v-tab--selected {
+  background-color: rgb(var(--v-theme-primary));
+}
+>>>.v-tab .v-btn__content {
   text-align: left;
   display: block;
 }
 
->>> div.v-toolbar__extension {
-  /*background-color: rgb(var(--v-theme-surface));*/
+>>>div.v-toolbar__extension {
+  background-color: rgb(var(--v-theme-primary));
 }
 
 </style>
